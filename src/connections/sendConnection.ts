2458ae5b44c651e13f22c06748a62e18b0612d13
Tanmay <144552425+babyo77@users.noreply.github.com>
2025-02-23 04:18:07 +0530

    Update sendConnection.ts

--- src/connections/sendConnection.ts
+++ src/connections/sendConnection.ts
@@ -4,47 +4,35 @@ import { Request, Response } from "express";
 import { INITIALIZE_BROWSER } from "../playwright.config";
 
 const initiateConnection = async (page: Page, message: string) => {
-  await delay(page);
   try {
     const messageButton = page
-      .locator('button:has(span.artdeco-button__text:text("Connect"))')
+      .locator('button:has(span:text("Connect"))')
       .nth(1);
     try {
-      await messageButton.isEnabled();
-      await messageButton.hover();
-      await messageButton.click();
+      await Promise.all([
+        messageButton.click(),
+        page.waitForResponse((response) => response.url().includes("connect")),
+      ]);
     } catch (error) {
-      const moreButton = await page
+      const moreButton = page
         .getByRole("button", { name: "More actions" })
         .first();
-
-      console.log("more button found");
       await moreButton?.click();
-      console.log("more button clicked");
-      await delay(page);
-      // Find and click Connect option
-      const connectButton = await page
+
+      const connectButton = page
         .locator("div.artdeco-dropdown__item", {
           hasText: "Connect",
         })
         .nth(1);
-      console.log("connect button found");
-      await connectButton?.click().catch((error) => {
-        console.log(error);
-      });
+      await connectButton?.click();
     }
 
-    const addNoteButton = await page
+    const addNoteButton = page
       .getByRole("button", {
         name: "Send without a note",
       })
       .first();
 
-    console.log("add note button found");
-    await addNoteButton?.hover();
-    console.log("add note button hovered");
-    await delay(page);
-    console.log("delay done");
     await addNoteButton?.click();
   } catch (error) {
     throw new Error("Connection request already pending");
@@ -67,9 +55,10 @@ export const sendConnectionRequest = async (req: Request, res: Response) => {
   }
   const page = await context.newPage();
   try {
-    await page.goto(linkedinUrl, {
-      waitUntil: "domcontentloaded",
-    });
+    await Promise.all([
+      page.goto(linkedinUrl),
+      page.waitForLoadState("domcontentloaded"),
+    ]);
     await initiateConnection(page, sendMessage);
     return res.status(200).json({ message: "Connection request sent" });
   } finally {
