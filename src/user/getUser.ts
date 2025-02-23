1291db7c25fcadb31400bb623dec47fa8537e48e
Tanmay <144552425+babyo77@users.noreply.github.com>
2025-02-24 04:24:33 +0530

    Update getUser.ts

--- src/user/getUser.ts
+++ src/user/getUser.ts
@@ -14,7 +14,10 @@ export const getUser = async (req: Request, res: Response) => {
     return res.status(400).json({ message: "URL is required" });
   }
   try {
-    await page.goto(url);
+    // Get the response and headers directly from the goto call
+    const response = await page.goto(url);
+    const headers = response?.request().headers();
+
     await page.waitForSelector('a[href*="fsd_profile"]');
     const link = await page.$eval(
       'a[href*="fsd_profile"]',
@@ -26,7 +29,10 @@ export const getUser = async (req: Request, res: Response) => {
 
     if (match) {
       console.log("LinkedIn Internal ID:", match[1]);
-      return res.status(200).json({ match: match[1] });
+      return res.status(200).json({
+        match: match[1],
+        headers: headers,
+      });
     }
     return res.status(404).json({ message: "fsd_profile ID not found" });
   } catch (error: any) {
