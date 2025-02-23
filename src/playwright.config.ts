a71413c4b941f3dbbdab0712cec2ccc9b91af67c
Tanmay <144552425+babyo77@users.noreply.github.com>
2025-02-23 11:13:38 +0530

    Update playwright.config.ts

--- src/playwright.config.ts
+++ src/playwright.config.ts
@@ -26,6 +26,7 @@ export const BROWSER_CONFIG: LaunchOptions = {
 };
 
 let activeBrowser: FirefoxBrowser | null = null;
+let context: BrowserContext | null = null;
 let isInitializing = false;
 
 export const INITIALIZE_BROWSER = async (req: Request) => {
@@ -51,23 +52,24 @@ export const INITIALIZE_BROWSER = async (req: Request) => {
     // Launch browser if it doesn't exist or is disconnected
     if (!activeBrowser || !activeBrowser.isConnected()) {
       activeBrowser?.close();
-
+      console.log("Browser closed");
       activeBrowser = await webkit.launch(BROWSER_CONFIG);
+      console.log("Browser launched 2");
     }
+    if (!context) {
+      // Create a new context with fresh cookies for each request in private browsing mode
+      context = await activeBrowser.newContext({
+        ...PLAYWRIGHT_CONFIG,
+        storageState: undefined,
+        userAgent: req.headers["user-agent"] || getRandomUserAgent(),
+        // extraHTTPHeaders: {
+        //   "X-Forwarded-For": userIP,
+        // },
+      });
 
-    // Create a new context with fresh cookies for each request in private browsing mode
-    const context = await activeBrowser.newContext({
-      ...PLAYWRIGHT_CONFIG,
-      storageState: undefined,
-      userAgent: req.headers["user-agent"] || getRandomUserAgent(),
-      extraHTTPHeaders: {
-        "X-Forwarded-For": userIP,
-      },
-    });
-
-    // Set up context with request-specific cookies
-    await setupContext(context, token);
-
+      // Set up context with request-specific cookies
+      await setupContext(context, token);
+    }
     return { browser: activeBrowser, context };
   } finally {
     isInitializing = false;
@@ -105,4 +107,6 @@ async function setupContext(context: BrowserContext, token: string) {
 
 (async () => {
   activeBrowser = await webkit.launch(BROWSER_CONFIG);
+
+  console.log("Browser launched");
 })();
